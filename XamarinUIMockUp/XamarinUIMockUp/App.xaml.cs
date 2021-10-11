d18e396b1094ed6ab76e150f94f0fd3dab2314e9
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2021-10-11 15:13:05 -0400

    Add project files.

---
+++ XamarinUIMockUp/XamarinUIMockUp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinUIMockUp.Views;
+
+namespace XamarinUIMockUp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnInitialized()
+        {
+            NavigationService.NavigateAsync("MainTab");
+        }
+
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainTabbedPage>("MainTab");
+        }
+    }
+}
